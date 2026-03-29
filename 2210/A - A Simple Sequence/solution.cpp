24f5b5f832cabdd8a6e2389bf071ef51adbc5d43
Amish Aryan <amisharyan248@gmail.com>
2026-03-29 20:19:39 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; int main() {    ios::sync_with_stdio(false);    cin.tie(nullptr);     int t;    cin >> t;     while (t--) {        int n;        cin >> n;                 cout << 2 << " ";               for (int i = n; i >= 3; i--) {            cout << i << " ";        }                 cout << 1 << "\n";    }     return 0;}
